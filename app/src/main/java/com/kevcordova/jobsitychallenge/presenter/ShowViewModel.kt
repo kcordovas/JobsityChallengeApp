9d2ceaaa086a049c0be4321990ed7e6d0cdedff7
kevc77 <kcordovas77@gmail.com>
2022-06-26 14:21:32 -0500

    Show all Serie list with ShowViewModel.kt and ShowListFragment.kt and your Use case

--- app/src/main/java/com/kevcordova/jobsitychallenge/presenter/ShowViewModel.kt
+++ app/src/main/java/com/kevcordova/jobsitychallenge/presenter/ShowViewModel.kt
@@ -1,6 +1,45 @@
 package com.kevcordova.jobsitychallenge.presenter
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kevcordova.jobsitychallenge.model.mappers.toShowRecyclerViewItem
+import com.kevcordova.jobsitychallenge.ui.adapters.base.BaseRecyclerViewItem
+import com.kevcordova.jobsitychallenge.usescases.GetAllShowUseCase
+import kotlinx.coroutines.launch
+import java.lang.NullPointerException
 
 class ShowViewModel : ViewModel() {
+    private var allShowUseCase : GetAllShowUseCase? = null
+
+    private val _showListEvent = MutableLiveData<Event<ShowListNavigation>>()
+    val showListEvent: LiveData<Event<ShowListNavigation>> get() = _showListEvent
+
+    fun setUseCase(getAllShowUseCase: GetAllShowUseCase) {
+        this.allShowUseCase = getAllShowUseCase
+    }
+
+    fun listShowAll() {
+        _showListEvent.value = generateEvent(ShowListNavigation.ShowLoading)
+        viewModelScope.launch {
+            val list = allShowUseCase?.invoke()
+            if (list.isNullOrEmpty()) {
+                _showListEvent.value = generateEvent(ShowListNavigation.ShowListError(NullPointerException("List is Null or Empty")))
+            } else {
+                _showListEvent.value = generateEvent(ShowListNavigation.ShowListAsRecyclerViewItem(list.map { it.toShowRecyclerViewItem() }))
+            }
+            _showListEvent.value = generateEvent(ShowListNavigation.HideLoading)
+        }
+    }
+
+    private fun <T> generateEvent(nav: T) : Event<T> = Event(nav)
+
+    // Navigation
+    sealed class ShowListNavigation {
+        data class ShowListError(val throwable: Throwable) : ShowListNavigation()
+        data class ShowListAsRecyclerViewItem(val showList: List<BaseRecyclerViewItem.ShowRecyclerViewItem>) : ShowListNavigation()
+        object ShowLoading : ShowListNavigation()
+        object HideLoading : ShowListNavigation()
+    }
 }
--- app/src/main/java/com/kevcordova/jobsitychallenge/ui/fragment/home/ShowListFragment.kt
+++ app/src/main/java/com/kevcordova/jobsitychallenge/ui/fragment/home/ShowListFragment.kt
@@ -1,5 +1,6 @@
 package com.kevcordova.jobsitychallenge.ui.fragment.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,9 +9,20 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import com.example.baseandroidmodulekevcordova.extensions.showShortToast
 import com.kevcordova.jobsitychallenge.R
+import com.kevcordova.jobsitychallenge.api.ShowRequest
+import com.kevcordova.jobsitychallenge.api.ShowRetrofitDataSource
+import com.kevcordova.jobsitychallenge.data.RemoteShowDataSource
+import com.kevcordova.jobsitychallenge.data.ShowRepository
 import com.kevcordova.jobsitychallenge.databinding.FragmentShowListBinding
+import com.kevcordova.jobsitychallenge.presenter.Event
 import com.kevcordova.jobsitychallenge.presenter.ShowViewModel
+import com.kevcordova.jobsitychallenge.ui.activity.ShowDetailsActivity
+import com.kevcordova.jobsitychallenge.ui.adapters.ShowListRecyclerViewAdapter
+import com.kevcordova.jobsitychallenge.ui.adapters.base.BaseRecyclerViewItem
+import com.kevcordova.jobsitychallenge.usescases.GetAllShowUseCase
 
 /**
  * A simple [Fragment] subclass.
@@ -19,6 +31,19 @@ import com.kevcordova.jobsitychallenge.presenter.ShowViewModel
  */
 class ShowListFragment : Fragment() {
 
+    private val remoteShowDataSource: RemoteShowDataSource by lazy {
+        ShowRetrofitDataSource(ShowRequest)
+    }
+    private val showRepository: ShowRepository by lazy {
+        ShowRepository(remoteShowDataSource)
+    }
+
+    private val getAllShowUseCase: GetAllShowUseCase by lazy {
+        GetAllShowUseCase(showRepository)
+    }
+
+    private val showListRecyclerViewAdapter = ShowListRecyclerViewAdapter()
+
     private val showSharedViewModel: ShowViewModel by activityViewModels()
     private lateinit var binding: FragmentShowListBinding
 
@@ -26,7 +51,8 @@ class ShowListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_show_list, container, false)
+        binding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.fragment_show_list, container, false)
         binding.fragment = this
         // Inflate the layout for this fragment
         return binding.root
@@ -35,6 +61,33 @@ class ShowListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.show_list)
+        binding.showListRecyclerview.adapter = showListRecyclerViewAdapter
+
+        showSharedViewModel.showListEvent.observe(
+            viewLifecycleOwner,
+            Observer(this::manageNavigationEvents)
+        )
+        showSharedViewModel.run {
+            setUseCase(getAllShowUseCase)
+            listShowAll()
+        }
+    }
+
+    private fun manageNavigationEvents(events: Event<ShowViewModel.ShowListNavigation>) {
+        events.getContentIfNotHandle()?.let { nav ->
+            when (nav) {
+                is ShowViewModel.ShowListNavigation.ShowListAsRecyclerViewItem -> nav.run {
+                    showListRecyclerViewAdapter.items = showList
+                }
+                is ShowViewModel.ShowListNavigation.ShowListError -> nav.run {
+                    activity?.showShortToast(throwable.message ?: "Error on list")
+                }
+                ShowViewModel.ShowListNavigation.HideLoading -> binding.progressLinearShowList.visibility =
+                    View.GONE
+                ShowViewModel.ShowListNavigation.ShowLoading -> binding.progressLinearShowList.visibility =
+                    View.VISIBLE
+            }
+        }
     }
 
     fun filter() {
